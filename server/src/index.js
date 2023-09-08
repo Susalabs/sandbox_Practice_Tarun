4c073e350ef8e936908fac9f4eea06c622523af4
tarun291 <tarunkumar222020@gmail.com>
2023-09-08 10:26:26 +0530

    fixes

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
 const db = require('./models/index')
-// const ApiRoutes = require('./routes/index');
+const ApiRoutes = require('./routes/index');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', ApiRoutes);
+app.use('/api', ApiRoutes);
 const PORT = 3000;
 app.listen(PORT, () => {
     db.sequelize.sync().then(() => {
--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const UserController = require('../../controllers/user-controller')
+const UserController = require('../../controller/user-controller')
 const router = express.Router();
 
 router.post('/users', UserController.create);
